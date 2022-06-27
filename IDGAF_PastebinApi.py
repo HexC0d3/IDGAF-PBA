67e9ad17a39e86fa522b83fe926bc04fcb491b77
HexC0d3 <himanshu12bhatt@gmail.com>
2022-06-28 01:55:33 +0530

    minor correction to output file

--- IDGAF_PastebinApi.py
+++ IDGAF_PastebinApi.py
@@ -17,9 +17,7 @@ parser = argparse.ArgumentParser(
     Version:'''+version+ ''' |
     Code by: HexC0d3
     '''))
-# parser.add_argument('--version', action='version', version="%(prog)s \nVersion: "+version)
-# parser.add_argument('-s',type=str, help="Keyword to Search" )
-# parser.add_argument('-iL',type=str, help="Input Text File containing list of Keywords" )
+
 inputGroup = parser.add_mutually_exclusive_group()
 inputGroup.add_argument('-s' , '--search', type=str, help="Keyword to Search")
 inputGroup.add_argument('-iL', '--input', type=str, help="Input Text File containing list of Keywords")
@@ -27,8 +25,6 @@ args = parser.parse_args()
 
 # TODO: 
 # 1. Add File check for all Required SRC files 
-# 2. Implement argParse
-# O. Output to a file
 # 4. Input Sanitization
 
 # Set Keyword a single query or file
@@ -37,28 +33,57 @@ if (args.search):
 elif (args.input):
     keyword = args.input
 
+
 def get_psDump(keyword):
     
     # -- Check for Output directory, if not exist it creates --
     if os.path.exists('Outputs') == 0:
-        os.makedirs('Outputs')
-
+        os.makedirs('Outputs') 
+    
     # -- Open a write mode file --
-    psOutFile = open('Outputs/output_'+ datetime.now().strftime("%d%m%Y-%H%M%S")+'.csv','w')
+    psOutFile = open('Outputs/output_'+ datetime.now().strftime("%d%m%Y-%H%M%S")+'.csv','a')
     psOutFile.writelines('KEYWORD,URL\n')
 
-    # -- Check for bin dumps in psdump --
-    dumpList = PsDump.check_psDump(keyword)
+    # -- Querying from a File (if arg passed) --
+    # -- Check if File exist, if exists File Arg was passed else keyword query.
+    if (os.path.exists(keyword)):
+        inFile = open(keyword,'r')
+        queries = inFile.readlines()
+        
+        for query in queries:
+            dumpList = PsDump.check_psDump(query)
+            if dumpList == []:
+                print("Keyword: "+query+ "\nNo result Found !!!\nOmitting saving this to the output")
+            else:
+                print('Keyword: '+query)
+                for urls in dumpList:
+                    print(urls) 
+                    psOutFile.writelines(query+','+urls+"\n")
+        # -- Close opened input file -- 
+        inFile.close()
 
-    # -- Check if List is empty --
-    if dumpList == []:
-        print("Keyword: "+keyword+ "\nNo result Found !!!\nOmitting saving this to the output")
     
-    # -- Print and save output
-    else:
-        for urls in dumpList:
-            print(urls) 
-            psOutFile.writelines(keyword+','+urls+"\n")
+    else : 
+        # -- Check for bin dumps in psdump --
+        dumpList = PsDump.check_psDump(keyword)
+
+        # -- Check if List is empty --
+        if dumpList == []:
+            print("Keyword: "+keyword+ "\nNo result Found !!!\nOmitting saving this to the output")
+        
+        # -- Print and save output
+        else:
+            print('Keyword: '+keyword)
+            for urls in dumpList:
+                print(urls) 
+                psOutFile.writelines(keyword+','+urls+"\n")
+    
+    # -- Close all open files --
+    psOutFile.close()
+    
+
+
+
 
 
 
